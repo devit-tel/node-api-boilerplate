a4dcd91568209b5836f70bf368d151a6d40c2ae9
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-05-10 18:31:17 +0700

    - remove database connection code - restructure of database directory

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ import path from 'path'
 import Koa from 'koa'
 import { load } from '@spksoft/koa-decorator'
 import config from './config'
-import mongooseClient from './libraries/database'
+// import mongooseClient from './libraries/database'
 import { getLogger } from './libraries/logger'
 import { NotFoundError, ErrorCode } from './libraries/error'
 import {
@@ -37,12 +37,12 @@ import {
       methodNotAllowed: () => new NotFoundError('The resquested uri does not match to any route tables', ErrorCode.URI_NOT_FOUND.CODE)
     }))
 
-    // Connect to database
-    let dbClient = null
-    if (config.database.databaseURL !== undefined && config.database.databaseURI !== null) {
-      dbClient = await mongooseClient(databaseConfig)
-      logger(`Connected to ${dbClient.host}:${dbClient.port}/${dbClient.name}`)
-    }
+    // // Connect to database
+    // let dbClient = null
+    // if (config.database.databaseURL !== undefined && config.database.databaseURI !== null) {
+    //   dbClient = await mongooseClient(databaseConfig)
+    //   logger(`Connected to ${dbClient.host}:${dbClient.port}/${dbClient.name}`)
+    // }
 
     app.listen(config.system.port)
     logger(`starting server on port ${config.system.port}`)
