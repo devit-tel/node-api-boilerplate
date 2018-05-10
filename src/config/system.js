13808cb098f7724c9f3f8cccb3822d31814206e2
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-05-10 18:11:43 +0700

    fix type error

--- src/config/system.js
+++ src/config/system.js
@@ -1,4 +1,6 @@
 export default {
+  graylogErrorStack: (process.env.GRAYLOG_ERROR_STACK === 'true'),
+  responseErrorStack: (process.env.RESPONSE_ERROR_STACK === 'true'),
   nodeENV: process.env.NODE_ENV,
   baseURI: process.env.BASE_URI,
   projectName: process.env.PROJECT_NAME,
