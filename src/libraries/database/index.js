0305b9948929551fac820c544183f3610f39218c
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-05-10 18:12:02 +0700

    add logger to database connection

--- src/libraries/database/index.js
+++ src/libraries/database/index.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import { getLogger } from '../logger'
+
+const logger = getLogger('app:database')
 
 export default (uri) => new Promise((resolve, reject) => {
   mongoose.connection
     .on('error', error => reject(error))
-    .on('close', () => console.log('Database connection closed.'))
+    .on('close', () => logger("database connection close."))
     .once('open', () => resolve(mongoose.connections[0]))
 
   mongoose.connect(uri)
