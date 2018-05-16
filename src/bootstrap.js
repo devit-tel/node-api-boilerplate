37731738affa2d2adeacc7f29580d4a859072a14
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-05-16 16:51:48 +0700

    use promise instend

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,43 +12,52 @@ import {
   cors,
   accessLogger,
   errorHandler,
-  responseFormatter
+  responseFormatter,
 } from './middlewares'
 
-(async () => {
-  try {
-    const logger = getLogger('app:bootstrap')
-    const app = new Koa()
-    logger('set a middleware to main app')
+const logger = getLogger('app:bootstrap')
+const app = new Koa()
+logger('set a middleware to main app')
 
-    // Plug "system middlewares"
-    bodyParser(app)
-    compress(app)
-    cors(app)
-    accessLogger(app)
-    errorHandler(app)
-    responseFormatter(app)
+// Plug "system middlewares"
+bodyParser(app)
+compress(app)
+cors(app)
+accessLogger(app)
+errorHandler(app)
+responseFormatter(app)
 
-    // load router
-    const apiRouter = load(path.resolve(__dirname, 'controllers'), '.controller.js')
-    app.use(apiRouter.routes())
-    app.use(apiRouter.allowedMethods({
-      throw: true,
-      notImplemented: () => new NotFoundError('The resquested uri does not match to any route tables', ErrorCode.URI_NOT_FOUND.CODE),
-      methodNotAllowed: () => new NotFoundError('The resquested uri does not match to any route tables', ErrorCode.URI_NOT_FOUND.CODE)
-    }))
+// load router
+const apiRouter = load(path.resolve(__dirname, 'controllers'), '.controller.js')
+app.use(apiRouter.routes())
+app.use(
+  apiRouter.allowedMethods({
+    throw: true,
+    notImplemented: () =>
+      new NotFoundError(
+        'The resquested uri does not match to any route tables',
+        ErrorCode.URI_NOT_FOUND.CODE,
+      ),
+    methodNotAllowed: () =>
+      new NotFoundError(
+        'The resquested uri does not match to any route tables',
+        ErrorCode.URI_NOT_FOUND.CODE,
+      ),
+  }),
+)
 
-    // // Connect to database
-    let dbClient = null
-    if (config.database.databaseURI !== undefined && config.database.databaseURI !== null) {
-      dbClient = await mongooseClient(config.database.databaseURI)
+// // Connect to database
+if (config.database.databaseURI) {
+  mongooseClient(config.database.databaseURI)
+    .then(dbClient => {
       logger(`Connected to ${dbClient.host}:${dbClient.port}/${dbClient.name}`)
-    }
+    })
+    .catch(err => {
+      console.error('Unable to start server!', err)
+      process.exit(1)
+    })
+}
 
-    const server = app.listen(config.system.port)
-    logger(`starting server on port ${config.system.port}`)
-    gracefulShutdown(server)
-  } catch (err) {
-    console.error('Unable to start server!', err)
-  }
-})()
+const server = app.listen(config.system.port)
+logger(`starting server on port ${config.system.port}`)
+gracefulShutdown(server)
