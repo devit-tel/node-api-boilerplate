02ac83b338e2a4fb22fba5148ecdb132d62c640e
NV4RE <tosbodesz@gmail.com>
2019-05-15 20:41:54 +0700

    Add log notify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import 'babel-polyfill'
 import './errors'
+import logger from './libraries/logger'
 import './bootstrap'
+
+logger.trace('Log "trace" enabled')
+logger.debug('Log "debug" enabled')
+logger.info('Log "info" enabled')
+logger.warn('Log "warn" enabled')
+logger.error('Log "error" enabled')
