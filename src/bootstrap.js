2dbdfff55e3ff5ca35ce2877b13a42e2c111ee75
NV4RE <tosbodesz@gmail.com>
2019-05-22 19:16:14 +0700

    Fix NotImplemented not working

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,7 +6,6 @@ import cors from '@koa/cors'
 import { load } from '@spksoft/koa-decorator'
 import gracefulShutdown from 'http-graceful-shutdown'
 import config from './config'
-import errors from './errors'
 import { createLogger } from './libraries/logger'
 import requestId from './middlewares/requestId'
 import accessLogger from './middlewares/accessLogger'
@@ -28,13 +27,6 @@ app.use(responseFormatter)
 logger.debug('Loading routers')
 const apiRouter = load(path.resolve(__dirname, 'controllers'), 'controller.js')
 app.use(apiRouter.routes())
-app.use(
-  apiRouter.allowedMethods({
-    throw: true,
-    notImplemented: errors.notImplemented,
-    methodNotAllowed: errors.methodNotAllowed,
-  }),
-)
 
 const server = app.listen(config.server.port, config.server.host, undefined, () => {
   logger.debug(`Server started at ${config.server.host}:${config.server.port}`)
--- src/middlewares/responseFormatter.js
+++ src/middlewares/responseFormatter.js
@@ -1,5 +1,8 @@
+import errors from '../errors'
+
 export default async (ctx, next) => {
   await next()
+  if (ctx.status === 404) throw new errors.NotImplemented('Route not found')
   ctx.body = {
     success: true,
     data: ctx.body,
