44e6573bf65d63a9a873f1e5b99a4c43e9f4ad5f
NV4RE <tosbodesz@gmail.com>
2019-05-23 16:21:28 +0700

    Fix mongoose connect error

--- src/libraries/mongoose/index.js
+++ src/libraries/mongoose/index.js
@@ -3,31 +3,22 @@ import { createLogger } from '../logger'
 import config from '../../config'
 import { EXIT_CODES } from '../../constants/app'
 
+mongoose.Promise = global.Promise
+
 const logger = createLogger('app:database')
 
-const mongooseConnector = (uri, options) =>
-  new Promise((resolve, reject) => {
-    mongoose.connection
-      .once('error', error => {
-        logger.error(error)
-        reject(error)
-      })
-      .once('connected', () => {
-        logger.info('connected')
-        mongoose.connection
-          .off('error')
-          .on('error', logger.error)
-          .on('disconnected', () => logger.warn('disconnected'))
-          .on('connected', () => logger.info('connected'))
-          .on('reconnectFailed', () => logger.warn('reconnect failed'))
-        resolve(mongoose.connections[0])
-      })
-
-    mongoose.connect(uri, options)
-  })
+const mongooseConnector = async (uri, options) => {
+  await mongoose.connect(uri, options)
+  mongoose.connection
+    .on('error', logger.error)
+    .on('disconnected', () => logger.warn('disconnected'))
+    .on('connected', () => logger.info('connected'))
+    .on('reconnectFailed', () => logger.warn('reconnect failed'))
+}
 
 if (config.clients.mongoDB.enabled) {
-  mongooseConnector(config.clients.mongoDB, config.clients.mongoDB.options).catch(() => {
+  mongooseConnector(config.clients.mongoDB.uri, config.clients.mongoDB.options).catch(error => {
+    logger.error(error)
     process.exit(EXIT_CODES.MONGOOSE_ERROR)
   })
 }
