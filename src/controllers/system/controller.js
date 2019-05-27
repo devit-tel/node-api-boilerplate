a6962406346a039872307eb362a908e12b0ca62e
NV4RE <tosbodesz@gmail.com>
2019-05-27 17:24:39 +0700

    Remove a dependency

--- src/controllers/system/controller.js
+++ src/controllers/system/controller.js
@@ -1,10 +1,16 @@
 import { HttpMethod, route } from '@sendit-th/koa-decorator'
 import getHealth from '../../domains/system/health'
+import getMonitor from '../../domains/system/monitor'
 
 @route('/system')
 export default class SystemController {
   @route('/health', HttpMethod.GET)
   health() {
     return getHealth()
+  }
+
+  @route('/monitor', HttpMethod.GET)
+  monitor() {
+    return getMonitor()
   }
 }
