d1082708485eba462c8f2617ba02806f906e3053
NV4RE <tosbodesz@gmail.com>
2019-05-28 12:38:22 +0700

    add example

--- src/libraries/rascal/subscribers/example/subscriber.js
+++ src/libraries/rascal/subscribers/example/subscriber.js
@@ -1,8 +1,9 @@
 // This is an example file, please remove
 export const SUBSCRIPTIONS_NAME = 'demo'
 
-export const handler = (message, respond) => {
+export const handler = async (message, respond) => {
   console.log(message)
+  await new Promise(resolve => setTimeout(() => resolve(), 5000))
   // respond.nack(new Error('Nope'))
   respond.ack()
 }
